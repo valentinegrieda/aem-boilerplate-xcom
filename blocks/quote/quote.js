f979a3a745fc2dc4ae24977e94a0827d8958006d
valentinegrieda <78785945+valentinegrieda@users.noreply.github.com>
2026-01-29 17:09:09 +0800

    Update quote.js

--- blocks/quote/quote.js
+++ blocks/quote/quote.js
@@ -1,1 +1,7 @@
-js
+export default function decorate(block) {
+  const [quoteWrapper] = block.children;
+
+  const blockquote = document.createElement('blockquote');
+  blockquote.textContent = quoteWrapper.textContent.trim();
+  quoteWrapper.replaceChildren(blockquote);
+}
